9c5e0ba0cfb6faa91e8fb9535e42c8371c9eb32a
Rohit Jain <rohit.jain@pataa.com>
2022-01-12 12:46:54 +0530

    Intial Commit

---
+++ sayhello/src/main/java/com/pataa/sayhello/GreetUser.java
@@ -0,0 +1,13 @@
+package com.pataa.sayhello;
+
+public class GreetUser {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
